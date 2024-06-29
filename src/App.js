6122de5f62c1f62955862fc11a1bf984ee421789
Siddhesh Gawade <siddheshgawade61@gmail.com>
2024-06-29 21:03:22 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+// import * as e from 'express';
 
 function App() {
+  let [user, setUser] = useState({
+    username: "",
+    password: ""
+  })
+  let [error, setError] = useState(false);
+  let [success, setSuccess] = useState(false);
+
+
+  let handleChange = (e) => {
+    setUser((prev=>{
+      return{
+        ...prev,
+        username: e.target.value
+      }
+    }))
+  }
+
+  let changePassword = (e)=>{
+    setUser((prev)=>{
+      return{
+        ...prev,
+        password: e.target.value
+      }
+    })
+  }
+
+  //handle submit
+  let handleSubmit = (e)=>{
+    e.preventDefault();
+    if(user.username === "admin" && user.password === "admin"){
+      setSuccess(true);
+      setError(false);
+    }else{
+      setError(true);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login Page</h1>
+      {error && <p>Invalid useranme or password</p>}
+
+      {success? <p>Welcome, user!</p>:
+      <form onSubmit={handleSubmit}>
+        <label>Username: </label>
+        <input type="text" name="username" required onChange={handleChange} />
+        <br />
+        <label>Password: </label>
+        <input type="password" name="password" required onChange={changePassword} />
+        <br/>
+        <input type="submit" value="Login" />
+      </form>
+      }
     </div>
   );
 }
